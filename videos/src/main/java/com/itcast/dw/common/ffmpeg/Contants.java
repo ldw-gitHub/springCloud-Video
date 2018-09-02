255d1bada99a1d441c43e79cb221c473887fa9d6
ldw-gitHub <git@github.com:ldw-gitHub/springbootshop.git>
2018-09-02 20:33:56 +0800

    upload videotype

---
+++ videos/src/main/java/com/itcast/dw/common/ffmpeg/Contants.java
@@ -0,0 +1,22 @@
+package com.itcast.dw.common.ffmpeg;
+
+public class Contants {
+	/**
+	 * @Description:(3.工具类主类)设置转码工具的各个路径
+	 * @param:@param args
+	 * @return:void
+	 * @author:Zoutao
+	 * @date:2018-6-22
+	 * @version:V1.0
+	 */
+
+	public static final String ffmpegpath = "D:/ffmpeg/bin/ffmpeg.exe"; // ffmpeg工具安装位置
+	public static final String mencoderpath = "D:/ffmpeg/bin/mencoder"; // mencoder工具安装的位置
+
+	public static final String videofolder = "E://Projectpicture/websiteimages/temp/"; // 需要被转换格式的视频目录
+	public static final String videoRealPath = "E://Projectpicture/websiteimages/temp/"; // 需要被截图的视频目录
+
+	public static final String targetfolder = "E://Projectpicture/websiteimages/finshvideo/"; // 转码后视频保存的目录
+	public static final String imageRealPath = "E://Projectpicture/websiteimages/finshimg/"; // 截图的存放目录
+
+}
